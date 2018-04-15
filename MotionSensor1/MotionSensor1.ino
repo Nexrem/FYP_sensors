7368571ea3dc83c9e974d850f361f2693b2f8845
Nexrem <lukas.trinkunas@gmail.com>
2018-04-15 11:47:06 +0100

    changing code for twitter testing

--- MotionSensor1/MotionSensor1.ino
+++ MotionSensor1/MotionSensor1.ino
@@ -1,31 +1,23 @@
-#include <ThingSpeak.h>
+//#include <ThingSpeak.h>
 #include <Ethernet.h>
 #include <SPI.h>
 //#include <DateTime.h>
 //#include <DateTimeStrings.h>
 
-/*
- * PIR sensor tester
- */
-
 byte mac[] = { 0xD4, 0x28, 0xB2, 0xFF, 0xA0, 0xA1 }; // Must be unique on local network
  
-int ledPin = 13;                // choose the pin for the LED
+//int ledPin = 13;                // choose the pin for the LED
 int inputPin = 2;               // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
 int val = 0;                    // variable for reading the pin status
 
-// Timer set up
-/*#define TIME_MSG_LEN  11   // time sync to PC is HEADER and unix time_t as ten ascii digits
-#define TIME_HEADER  255   // Header tag for serial time sync message */
 
 // ThingSpeak Set up
 char thingSpeakAddress[] = "api.thingspeak.com";
-
-unsigned long myChannelNumber = CH-n;
-String writeAPIKey = "thingspeak";
-String thingtweetAPIKey = "thingtweet";
-const int updateThingSpeakInterval = 16 * 1000; // Time Interval in miliseconds to update ThingSpeak
+String thingtweetAPIKey = "4A7JNXU99X5OJGC1";
+//unsigned long myChannelNumber = 404697;
+//String writeAPIKey = "SJCZ65ABE049F2BF";
+//const int updateThingSpeakInterval = 16 * 1000; // Time Interval in miliseconds to update ThingSpeak
 
 // Variable Setup
 long lastConnectionTime = 0; 
@@ -39,17 +31,23 @@ void setup() {
   pinMode(inputPin, INPUT);     // declare sensor as input
  
   Serial.begin(9600);
+
+  
   startEthernet();
+
+  delay(1000);
+
+  updateTwitterStatus("Test started");
 }
  
 void loop(){
 
-    if (client.available())
+  if (client.available())
   {
     char c = client.read();
     Serial.print(c);
   }
-    if (!client.connected() && lastConnected)
+  if (!client.connected() && lastConnected)
   {
     Serial.println("...disconnected");
     Serial.println();
@@ -64,21 +62,19 @@ void loop(){
   
   val = digitalRead(inputPin);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
-  //  digitalWrite(ledPin, HIGH);  // turn LED ON
     if (pirState == LOW) {
       // we have just turned on
       Serial.println("Motion detected!");
       // We only want to print on the output change, not state
-      if(!client.connected() && (millis() - lastConnectionTime > updateThingSpeakInterval))
-        {
-        updateThingSpeak("field1="+val);
-        updateThingTweet("Motion detected in room");
-        }
+     // if(!client.connected() && (millis() - lastConnectionTime > updateThingSpeakInterval))
+       // {
+      //  updateThingSpeak("field1="+val);
+      updateTwitterStatus("Motion detected in room");
+       // }
         
       pirState = HIGH;
     }
   } else {
-   // digitalWrite(ledPin, LOW); // turn LED OFF
     if (pirState == HIGH){
       // we have just turned of
       Serial.println("Motion ended!");
@@ -88,56 +84,13 @@ void loop(){
   }
 }
 
-void updateThingSpeak(String tsData)
+void updateTwitterStatus(String tsData)
 {
   if (client.connect(thingSpeakAddress, 80))
-  {         
-    client.print("POST /update HTTP/1.1\n");
-    client.print("Host: api.thingspeak.com\n");
-    client.print("Connection: close\n");
-    client.print("X-THINGSPEAKAPIKEY: "+writeAPIKey+"\n");
-    client.print("Content-Type: application/x-www-form-urlencoded\n");
-    client.print("Content-Length: ");
-    client.print(tsData.length());
-    client.print("\n\n");
-
-    client.print(tsData);
-    
-    lastConnectionTime = millis();
-    
-    if (client.connected())
-    {
-      Serial.println("Connecting to ThingSpeak...");
-      Serial.println();
-      
-      failedCounter = 0;
-    }
-    else
-    {
-      failedCounter++;
-  
-      Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");   
-      Serial.println();
-    }
-    
-  }
-  else
-  {
-    failedCounter++;
-    
-    Serial.println("Connection to ThingSpeak Failed ("+String(failedCounter, DEC)+")");   
-    Serial.println();
-    
-    lastConnectionTime = millis(); 
-  }
-}
-
-void updateThingTweet (String tsData)
-{
-  if (client.connect(thingSpeakAddress, 80))
-  {         
+  { 
+    // Create HTTP POST Data
     tsData = "api_key="+thingtweetAPIKey+"&status="+tsData;
-
+            
     client.print("POST /apps/thingtweet/1/statuses/update HTTP/1.1\n");
     client.print("Host: api.thingspeak.com\n");
     client.print("Connection: close\n");
@@ -202,3 +155,48 @@ void startEthernet()
   delay(1000);
 }
 
+/*
+void updateThingSpeak(String tsData)
+{
+  if (client.connect(thingSpeakAddress, 80))
+  {         
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+writeAPIKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(tsData.length());
+    client.print("\n\n");
+
+    client.print(tsData);
+    
+    lastConnectionTime = millis();
+    
+    if (client.connected())
+    {
+      Serial.println("Connecting to ThingSpeak...");
+      Serial.println();
+      
+      failedCounter = 0;
+    }
+    else
+    {
+      failedCounter++;
+  
+      Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");   
+      Serial.println();
+    }
+    
+  }
+  else
+  {
+    failedCounter++;
+    
+    Serial.println("Connection to ThingSpeak Failed ("+String(failedCounter, DEC)+")");   
+    Serial.println();
+    
+    lastConnectionTime = millis(); 
+  }
+}
+*/
